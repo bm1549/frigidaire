e01cb9103952eece0117ab55c104a4cbc09c256b
Brian Marks <bm1549@nyu.edu>
2021-05-12 15:58:08 -0400

    Prep for 0.3 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='frigidaire',
-    version='0.2',
+    version='0.3',
     author="Brian Marks",
     description="Python API for the Frigidaire 2.0 App",
     long_description=long_description,
