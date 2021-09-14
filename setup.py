74d578afff5c8c9e9698dac7aa42642975462219
Chase Sterling <chase.sterling@gmail.com>
2021-09-14 12:07:46 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='frigidaire',
-    version='0.14',
+    version='0.15',
     author="Brian Marks",
     description="Python API for the Frigidaire 2.0 App",
     license="MIT",
